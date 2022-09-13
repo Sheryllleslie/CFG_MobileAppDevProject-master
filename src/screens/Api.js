0d58038b65ac428fd990185f9d2683820b1dd09c
Sheryl Leslie <107503443+Sheryllleslie@users.noreply.github.com>
2022-09-13 20:04:11 +0100

    Added search bar with api connection

--- src/screens/Api.js
+++ src/screens/Api.js
@@ -1,12 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
-import ApiCode from '../components/ApiCode.js'
+import { ActivityIndicator, FlatList, Text, View, Button } from 'react-native';
+import { Input, Icon } from '@rneui/themed';
+
 
 export default Api = () => {
+    const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const [query, setQuery] = useState('')
+
+  const getFood = async () => {
+     try {
+      const response = await fetch('https://api.calorieninjas.com/v1/nutrition?query='+ query, {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  'X-Api-Key': 'WyLfBylzlZh6YSJZVnmOwA==tnJx5aOYk5VTBx6X'},
+  });;
+      const json = await response.json();
+      setData(json.items);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getFood();
+  }, []);
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <ApiCode Query = '10oz onion and a tomato'/>
+      <Input
+      placeholder="Add item"
+      leftIcon={{ type: 'font-awesome', name: 'list' }}
+      onChangeText={(value) => setQuery(value)}
+      />
+      <Button title='submit' onPress={getFood}/>
+        {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.name}, {item.calories}</Text>
+          )}
+        />
+      )}
     </View>
   );
 };
