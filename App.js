f6ae1027d8315518a08b6a10c20029e3c06f40be
Sheryl Leslie <107503443+Sheryllleslie@users.noreply.github.com>
2022-09-16 13:53:46 +0100

    Updated the navigation so we have a bottom navigation bar.

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
-import Home from "./src/screens/Home.js";
+import Home from "./src/screens/home.js";
 import FoodLog from "./src/screens/FoodLog.js";
 import AddFood from "./src/screens/AddFood.js";
 import Api from "./src/screens/Api.js";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen name="FoodLog" component={FoodLog} />
-        <Stack.Screen name="AddFood" component={AddFood} />
-        <Stack.Screen name="Api" component={Api} />
-      </Stack.Navigator>
+<Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            }
+            if (route.name === 'FoodLog') {
+              iconName = focused ? 'fast-food' : 'fast-food-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{ title: "Welcome" }} />
+        <Tab.Screen name="FoodLog" component={FoodLog} />
+        <Tab.Screen name="AddFood" component={AddFood} />
+        <Tab.Screen name="Api" component={Api} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
